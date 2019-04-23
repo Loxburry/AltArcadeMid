d9728a88503f70f59dfaf0413c23c6977686b81c
Kenny Cox <thisisnotkenn@gmail.com>
2019-04-23 13:34:13 -0600

    Fixed sound

--- GameFile/src/Game/Scenes/Startscreen.js
+++ GameFile/src/Game/Scenes/Startscreen.js
@@ -8,12 +8,12 @@ class StartScreen extends Phaser.Scene {
     // takes the below function and attaches it to the serial port
    }
    preload(){
-       this.preload.audio('start',['../../start.mp3']);
-       this.preload.audio('index',['../../Low.mp3']);
-       this.preload.audio('middle',['../../Med_Low.mp3']);
-       this.preload.audio('ring',['../../Med_High.mp3']);
-       this.preload.audio('pinkie',['../../High.mp3']);
-       this.preload.audio('titleTheme',['../../titleTheme.mp3']);
+       this.load.audio('start',['./start.mp3']);
+       this.load.audio('index',['./Low.mp3']);
+       this.load.audio('middle',['./Med_Low.mp3']);
+       this.load.audio('ring',['./Med_High.mp3']);
+       this.load.audio('pinkie',['./High.mp3']);
+       this.load.audio('titleTheme',['./titleTheme.mp3']);
    }
    create(){ // HTML user interface, generate array of bullets
     this.overlay = document.querySelector('#start-screen');
@@ -40,6 +40,13 @@ class StartScreen extends Phaser.Scene {
         this.overlay.classList.add('hidden');
         this.scene.start('MainScene');
     }
+    // one button continue code
+    // if(handValues[0] == 1){
+    //     this.sound.play('start', { volume: 0.5 });// set volume between 0 and 1
+    //     this.overlay.classList.add('hidden');
+    //     this.scene.start('MainScene');
+    // }
+    
    }
 }
 
